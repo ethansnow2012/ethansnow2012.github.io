a62cb810601a8a0508755e40b60d1cf910654a29
ethansnow <ethansnow2012@gmail.com>
2022-03-30 16:35:14 +0800

    feat: blog v2 init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { MainPage } from 'containers/MainPage'
-import { RecolePage } from 'containers/RecolePage'
+
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <BrowserRouter >
         <Routes>
           <Route path='/' element={<MainPage/>}></Route>
-          <Route path='/recole' element={<RecolePage/>}></Route>
+          
         </Routes>
       </BrowserRouter>
     </div>
