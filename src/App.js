f291a2582a05dfcc59bd061fe3792240ea363b88
ethansnow <ethansnow2012@gmail.com>
2022-04-07 22:12:11 +0800

    feat: sign in flow

--- src/App.js
+++ src/App.js
@@ -4,20 +4,32 @@ import { MainPage } from 'containers/MainPage'
 import { ContentPage } from 'containers/ContentPage'
 // import { RootPage } from 'containers/Functional/RootPage'
 import { RootPageHoc } from 'hoc/factory/RootPageHoc'
+import { createContext } from 'react'
 
+export const globalContext = createContext(null)
 
 function App() {
   return (
     <div className="App">
-
-      <BrowserRouter >
-        <Routes>
-          <Route path='/' element={RootPageHoc(MainPage, ContentPage, {priority:'left'})}></Route>
-          {/* <Route path='/content1' element={<RootPage renderCode={'1'}/>}></Route>
-          <Route path='/content2' element={<RootPage renderCode={'2'}/>}></Route> */}
-          <Route path='/content/:id' element={RootPageHoc(MainPage, ContentPage, {priority:'right'})}></Route>
-        </Routes>
-      </BrowserRouter>
+      <globalContext.Provider value={
+          {
+            firebase:{
+              self:window.firebase,
+              firebaseGSignin: window.firebaseGSignin,
+              firebaseGSignout: window.firebaseGSignout
+            }
+          }
+        }>
+        
+        <BrowserRouter >
+          <Routes>
+            <Route path='/' element={RootPageHoc(MainPage, ContentPage, {priority:'left'})}></Route>
+            {/* <Route path='/content1' element={<RootPage renderCode={'1'}/>}></Route>
+            <Route path='/content2' element={<RootPage renderCode={'2'}/>}></Route> */}
+            <Route path='/content/:id' element={RootPageHoc(MainPage, ContentPage, {priority:'right'})}></Route>
+          </Routes>
+        </BrowserRouter>
+      </globalContext.Provider>
     </div>
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,20 +1,60 @@
 
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import styled from 'styled-components'
+import { globalContext } from 'App'
 
 const Styled = styled.div`
+    position: relative;
     padding-top:15vmin;
     padding-bottom:15vmin;
     color: white;
     background-color: var(--color-blue-visual-weight);
     justify-content: center;
     display: flex;
+    .control{
+        position: absolute;
+        right:0;
+        top:0;
+    }
+    .control .btn{
+        padding: 5px 8px;
+        cursor: pointer;
+    }
 `
 
 export function Header() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+    const {firebase} = useContext(globalContext)
+    const logIn = ()=>{
+        firebase.firebaseGSignin().then(()=>{
+            setIsLoggedIn(true)
+        })
+    }
+    const signOut = ()=>{
+        //firebase.firebaseGSignout()
+        setIsLoggedIn(false)
+    }
+    useEffect(()=>{
+        firebase.self.auth().onAuthStateChanged(function(user) {
+            if (user) {
+                setIsLoggedIn(true)
+            } else {
+                setIsLoggedIn(false)
+            }
+          });
+    }, [])
+    
     return (
         <Styled>
             xxxxxdddddxxxxx
+            <div className='control'> 
+                {
+                    isLoggedIn?
+                    <div className='btn' onClick={signOut}>SignOut</div>
+                    :
+                    <div className='btn' onClick={logIn}>LogIn</div>
+                }
+            </div>
         </Styled>
     )
 }
