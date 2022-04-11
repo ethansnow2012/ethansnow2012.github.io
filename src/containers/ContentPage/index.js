5663893dbfceeac30679a04b073eaf9b6d8fe63e
ethansnow <ethansnow2012@gmail.com>
2022-04-11 11:34:36 +0800

    fix: edit not use preventDefault

--- src/containers/ContentPage/index.js
+++ src/containers/ContentPage/index.js
@@ -144,15 +144,21 @@ export const ContentPage = forwardRef(function(props, ref) {
     }
     const handleKeyDown = useCallback((event)=>{
         console.log('handleKeyDown')
-        event.preventDefault();
+        let preventDefault = false
         let charCode = String.fromCharCode(event.which).toLowerCase();
         if((event.ctrlKey || event.metaKey) && charCode === 's') {
           //alert("CTRL+S Pressed");
+          preventDefault = true
           console.log('saving', )//get the main page ref  || pairContentRef={leftContentRef} 
         }else if((event.ctrlKey || event.metaKey) && charCode === 'c') {
+            preventDefault = true
           //alert("CTRL+C Pressed");
         }else if((event.ctrlKey || event.metaKey) && charCode === 'v') {
+            preventDefault = true
           //alert("CTRL+V Pressed");
+        }
+        if(preventDefault){
+            event.preventDefault();
         }
     }, [])
 
