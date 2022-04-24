66d3afa16bca47999ca79f024a6ccfd2c7f629c2
ethansnow <ethansnow2012@gmail.com>
2022-04-25 00:14:24 +0800

    fix: github hosting redirect issue

--- src/components/RouterMeta/index.js
+++ src/components/RouterMeta/index.js
@@ -2,14 +2,18 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom'
 
+let NODE_ENV = process.env.REACT_APP_NODE_ENV
+
 export function RouterMeta(props) {
     let navigate = useNavigate(); 
     useEffect(()=>{
-        console.log('RouterMeta navigate')
-        const href = window.location.href
-        const newPath = href.split('/?gh-pages-alter')[1]
-        if(newPath){
-            navigate(newPath, {replace: true})
+        if(NODE_ENV=='github_page'){
+            console.log('RouterMeta navigate')
+            const href = window.location.href
+            const newPath = href.split('/?gh-pages-alter')[1]
+            if(newPath){
+                navigate(newPath.replace('&','?'), {replace: true})
+            }
         }
     }, [])
     return (
